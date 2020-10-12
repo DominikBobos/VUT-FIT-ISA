d0afcbd9ec55ecfd0ab7797fcc466373aeb9aef1
Dominik Bobos <bobos.dominik@gmail.com>
2020-10-13 01:18:54 +0200

    Finished probably "just" documentation

--- ssl-sniffer.c
+++ ssl-sniffer.c
@@ -6,7 +6,7 @@
 ********************************************/
 
 
-#include "ssl-sniffer.h"
+#include "ssl-sniffer.h"        // My own header file   //
 #include <stdio.h>				//////////////////////////
 #include <signal.h>				//						//
 #include <stdlib.h> 			//						//
@@ -18,23 +18,25 @@
 #include <netdb.h>				//////////////////////////
 #include <arpa/inet.h>			//						//
 #include <pcap.h>				//						//
-#include <netinet/ip.h>			// Libs for sniffing 	//
+#include <netinet/ip.h>			// Libs for parsing 	//
 #include <netinet/tcp.h>		//						//
 #include <netinet/if_ether.h>	//						//
 #include <netinet/ip6.h> 		//////////////////////////
-//sudo apt-get install libpcap0.8-dev
+//Tested with libpcap0.8 to install use this -> sudo apt-get install libpcap0.8-dev
 
 
 tDLList connection_list; //list to track SSL/TLS connections
 
+/*
+ * Show all available devices to listen
+ * returns 0 on success
+ */
 int show_interfaces()
 {
 	char errbuf[PCAP_ERRBUF_SIZE]; 		//PCAP macro
 	pcap_if_t *alldevs, *dlist;
 	int i = 0;
-	  // Shows list of the all devices
-	if (pcap_findalldevs(&alldevs, errbuf) == -1)
-	{
+	if (pcap_findalldevs(&alldevs, errbuf) == -1) {  // Shows list of the all devices
 		fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
 		return -5;
 	}
@@ -46,12 +48,13 @@ int show_interfaces()
     printf("\nAll available interfaces to listen:\n");
     for (dlist = alldevs; dlist; dlist = dlist->next) {
         printf("\t%d. %s", ++i, dlist->name);
-        if (dlist->description)
+        if (dlist->description) {
             printf(" (%s)\n", dlist->description);
-        else
+        }
+        else {
             printf("(No description)\n");
+        }
     }
-
 	return 0;
 }
 
@@ -71,9 +74,15 @@ void intHandler()
  */
 void print_help()
 {
-	puts("HELP:");
-	puts("-i <interface> interface, where the SSL monitor captures the connection");
-	puts("-r <file> pcapng file to get SSL connection from ");
+    printf("\t\t\t----------------------\n\t\t\tSSL connection monitor\n\t\t\t----------------------\n"
+           "The program catch SSL connection from SSlv2 to TLS1.2 version\n\n"
+           "OUTPUT FORMAT:\n\t<timestamp>,<client ip>,<client port>,<server ip>,<SNI>,<bytes>,<packets>,<duration sec>\n");
+	puts("USAGE:\n"
+      "\t-i [<interface name>|<interface number>] interface, where the SSL monitor captures the connection\n"
+      "\t-r <file> pcap/pcapng file to get SSL connection from \n"
+      "\nIf the program does not work. Launch it with sudo.\n"
+      "When both -i and -r are selected, only -i is in use.\n"
+      "Use no arguments to show available devices to listen.");
 }
 
 
@@ -109,8 +118,7 @@ int args_parse(int argc, char *argv[], char *iface, char *rfile)
 		}
 		switch (opt) {
 		case 'i':
-			if (strlen(optarg) > 40)
-			{
+			if (strlen(optarg) > 40) {
 				fprintf(stderr, "Parameter for interface could not be longer than 40 characters!\n");
 				return 1;
 			}
@@ -118,8 +126,7 @@ int args_parse(int argc, char *argv[], char *iface, char *rfile)
 			iface_bool = true;
 			break;
 		case 'r':
-			if (strlen(optarg) > 200)				
-			{
+			if (strlen(optarg) > 200) {
 				fprintf(stderr, "Parameter for pcapng file could not be longer than 200 characters!\n");
 				return 1;
 			}
@@ -211,18 +218,20 @@ char *readIPv6(struct in6_addr ip_addr)
 
 /*
  * Gets SNI from SSL packet of type Client-Hello
+ * returns NULL on fail
+ * returns char *SNI on success
  */
-char *get_TLS_SNI(unsigned char *buffer, int tcphdr_len)
+char *get_TLS_SNI(const u_char *buffer, int tcphdr_len)
 {
-    int x =  *(int_least8_t *)(&buffer[tcphdr_len + 43]);   //session ID length
+    int x =  *(int_least8_t *)(&buffer[tcphdr_len + 43]);                   //session ID length
     int y = ntohs(*(uint32_t *)(&buffer[tcphdr_len + 43 + x + 1])); // cipher suites length
-    int pos = tcphdr_len + 43 + x + 1 + y ;                         // position in byte array
+    int pos = tcphdr_len + 43 + x + 1 + y ;                                 // position in byte array
     int extenstion_length = ntohs(*(uint32_t *)(&buffer[pos + 4]));
-    int length = 0;                                                 // server name length
+    int length = 0;                                                         // server name length
     for (int i = 6; i < extenstion_length;){
-        if (ntohs(*(uint32_t *)(&buffer[pos + i])) == 0) {  // type server name (0x0000 == 0)
-            length = ntohs(*(uint32_t *)(&buffer[pos + i + 7]));  //server name length
-            pos = pos + i + 9;                                            //updates position - place where SNI is
+        if (ntohs(*(uint32_t *)(&buffer[pos + i])) == 0) {          // type server name (0x0000 == 0)
+            length = ntohs(*(uint32_t *)(&buffer[pos + i + 7]));    //server name length
+            pos = pos + i + 9;                                              //updates position - place where SNI is
             break;
         }
         else {
@@ -230,25 +239,28 @@ char *get_TLS_SNI(unsigned char *buffer, int tcphdr_len)
             i += ntohs(*(uint32_t *)(&buffer[pos + i])) + 2 ;
         }
     }
-    if (length < 1) { return NULL; }
-    char *SNI = malloc(length+1 * sizeof(char));
-    if (!SNI) {	return NULL; }
+    if (length < 1) { return NULL; }    // 0 length of SNI (to prevent malloc fail)
+    char *SNI = malloc(length+1 * sizeof(char));    // +1 because of '\0'
+    if (!SNI) {	return NULL; }                           // malloc fail
     for(int i = 0; i < length; i++){
-        SNI[i] = (char *)(buffer[pos + i]);
+        SNI[i] = (char)(buffer[pos + i]);
     }
     SNI[length] = '\0';
     return SNI;
 }
 
 
-// returns the real length of payload if there is more TLS packets sticked together
-// if no occurences it returns 0
-int loop_packet (const u_char *buffer, int tcphdr_len, unsigned int data_len, bool finding_TLS) {
+/*
+ * returns the real length of payload if there is
+ * more TLS packets sticked together in one TCP connection
+ * returns 0 if there are no more occurences of TLS packets.
+ * otherwise it returns the
+ */
+int loop_packet (const u_char *buffer, int tcphdr_len, unsigned int data_len) {
     int payload_size = 0;
-    if (finding_TLS == false)
-        tcphdr_len += 3;    //offset to not count the payload we have already counted
+    tcphdr_len += 3;    //offset used for to not count the payload we have already counted (jumps 3 bytes in buffer)
     for(int i = 0; i < (int)data_len -5; i++) {
-        if (((buffer[tcphdr_len + i] == 0x14) ||
+        if (((buffer[tcphdr_len + i] == 0x14) ||    //checks the buffer for another TLS packet in payload
              (buffer[tcphdr_len + i] == 0x15) ||
              (buffer[tcphdr_len + i] == 0x17)) &&
             (buffer[tcphdr_len + 1 + i] == 0x03 && (buffer[tcphdr_len + 2 + i] < 0x04))) {
@@ -259,7 +271,14 @@ int loop_packet (const u_char *buffer, int tcphdr_len, unsigned int data_len, bo
     return payload_size;
 }
 
-void ssl_connection (const u_char *buffer, unsigned data_len, int tcphdr_len, uint16_t src_port, uint16_t dst_port, char *src_addr, char *dst_addr, long time, long microsec)
+/*
+ *  Function parses the SSL header and saves the info to the pckt_info header
+ *  all the pckt_info variables are saved in connection_list.
+ */
+void ssl_connection (const u_char *buffer, unsigned data_len, int tcphdr_len,
+                     uint16_t src_port, uint16_t dst_port,
+                     char *src_addr, char *dst_addr,
+                     long time, long microsec)
 {
     pckt_info header;
     strcpy(header.src_addr, src_addr);
@@ -272,44 +291,48 @@ void ssl_connection (const u_char *buffer, unsigned data_len, int tcphdr_len, ui
     header.end_time = time;
     header.end_microsec = microsec;
 
-//        //https://stackoverflow.com/questions/3897883/how-to-detect-an-incoming-ssl-https-handshake-ssl-wire-format
-    if (buffer[tcphdr_len] == 0x16 && buffer[tcphdr_len + 5] == 0x01 &&
-        ntohs(src_port) != 443) { //Handshake type Client-Hello SSLv3 and higher
+    // INFO (knowledge) FROM
+    // SOURCE: https://stackoverflow.com/questions/3897883/how-to-detect-an-incoming-ssl-https-handshake-ssl-wire-format
+    // AUTHOR: Jakub
+    if (buffer[tcphdr_len] == 0x16 &&   //Handshake type Client-Hello SSLv3 and higher
+    buffer[tcphdr_len + 5] == 0x01 &&
+    ntohs(src_port) != 443) { //Handshake type Client-Hello SSLv3 and higher
         char *temp_SNI = get_TLS_SNI(buffer, tcphdr_len);
         if (!temp_SNI) {    //not found server name extension
             strcpy(header.sni, "(Could not find SNI)");
-        } else {
+        }
+        else {
             strcpy(header.sni, temp_SNI);
             free(temp_SNI);
         }
         header.ssl_ver = 1;
         header.packets = 1;
         header.size = ntohs(*(uint32_t *) (&buffer[tcphdr_len + 3]));
-        header.size += loop_packet(buffer, tcphdr_len, data_len, false);
+        header.size += loop_packet(buffer, tcphdr_len, data_len);
         DLInsertLast(&connection_list, header, ntohs(src_port));
-
-    } else if ((buffer[tcphdr_len + 3] == 0x00) && (buffer[tcphdr_len + 4] == 0x02) &&    //SSLv2
-               buffer[tcphdr_len + 2] == 0x01) { //handshake Client-Hello SSLv2
+    }
+    else if ((buffer[tcphdr_len + 3] == 0x00) && (buffer[tcphdr_len + 4] == 0x02) &&    //SSLv2
+               buffer[tcphdr_len + 2] == 0x01) { //Handshake Client-Hello SSLv2
         header.ssl_ver = 0;
         header.packets = 1;
-        header.size = ((buffer[tcphdr_len] & 0x7f) << 8 | buffer[tcphdr_len + 1]); //ntohs(buffer[]); //test it pls
+        header.size = *(int_least8_t *)(&buffer[tcphdr_len + 1]); //ntohs(buffer[]); //test it pls
         strcpy(header.sni, "(SSLv2 no SNI provided)");
         DLInsertLast(&connection_list, header, ntohs(src_port));
-    } else {
-
+    }
+    else {  //adds info to the current connection, such as size, increment packet count, timestamp
         for (DLFirst(&connection_list); connection_list.Act != NULL; DLSucc(&connection_list)) {
             if (connection_list.Act->port == ntohs(src_port) ||
-                connection_list.Act->port == ntohs(dst_port)) {
+            connection_list.Act->port == ntohs(dst_port)) {
                 connection_list.Act->data.end_time = time;
                 connection_list.Act->data.end_microsec = microsec;
                 connection_list.Act->data.packets += 1;
                 if (connection_list.Act->data.ssl_ver == 1) {
                     uint32_t length = *(uint32_t *) (&buffer[tcphdr_len + 3]);
                     connection_list.Act->data.size += ntohs(length); // add bytes to all data transfered
-                    connection_list.Act->data.size += loop_packet(buffer, tcphdr_len, data_len, false);
-                } else {
-                    connection_list.Act->data.size += ((buffer[tcphdr_len] & 0x7f) << 8 |
-                                                       buffer[tcphdr_len + 1]);  //SSLv2
+                    connection_list.Act->data.size += loop_packet(buffer, tcphdr_len, data_len);
+                }
+                else {
+                    connection_list.Act->data.size +=  *(int_least8_t *)(&buffer[tcphdr_len + 1]);  //SSLv2
                 }
                 break;
             }
@@ -323,65 +346,60 @@ void ssl_connection (const u_char *buffer, unsigned data_len, int tcphdr_len, ui
 *	source and destination port and
 *	source and destination ip's from IP header
 *   returns 0 on success and -10 when malloc failed
+*   if TCP packet carries SSL communication
+*   the function process the SSL packet
+*   further to the ssl_connection(...) function
 * 	It is modification from
 * 	SOURCE: https://gist.github.com/fffaraz/7f9971463558e9ea9545
 *	AUTHOR: Faraz Fallahi
 */
 int tcp_packet(long time, long microsec, const u_char *buffer, bool ipv6, unsigned int data_len)
 {
-    pckt_info header;
     int iphdr_len;
     char *temp_src = NULL;
     char *temp_dest = NULL;
 
-    if (ipv6 == true)
-    {
+    if (ipv6 == true) {
         struct ip6_hdr *iph = (struct ip6_hdr *)(buffer + sizeof(struct ether_header));
         iphdr_len = 40; //fixed size
         temp_src = readIPv6(iph->ip6_src);
         temp_dest = readIPv6(iph->ip6_dst);
     }
-    else
-    {
+    else {
         struct ip *iph = (struct ip *)(buffer + sizeof(struct ether_header));
-        iphdr_len = iph->ip_hl*4;
+        iphdr_len = (int)iph->ip_hl*4;
         temp_src = readIPv4(iph->ip_src);
         temp_dest = readIPv4(iph->ip_dst);
     }
     if (temp_src == NULL|| temp_dest == NULL) {	//malloc error
         return -10;
     }
-
     struct tcphdr *tcph = (struct tcphdr*)(buffer + iphdr_len + sizeof(struct ether_header));
-    unsigned tcp = iphdr_len + sizeof(struct ether_header);
     int tcphdr_len =  sizeof(struct ether_header) + iphdr_len + tcph->th_off*4 ;
-
-
 
     // Condition to find SSL packet in tcp payload
     // MODIFICATED function from:
     // SOURCE: https://www.netmeister.org/blog/tcpdump-ssl-and-tls.html
     // AUTHOR: jschauma
-    if ((((buffer[tcphdr_len] == 0x14) ||
+    if ((((buffer[tcphdr_len] == 0x14) ||                       //SSLv3 - TLS1.2
     (buffer[tcphdr_len] == 0x15) ||
     (buffer[tcphdr_len] == 0x17)) &&
     (buffer[tcphdr_len+1] == 0x03 && (buffer[tcphdr_len+2] < 0x04)))   ||
-    ((buffer[tcphdr_len] == 0x16) &&
+    ((buffer[tcphdr_len] == 0x16) &&                            //SSLv3 - TLS1.2 handshake
     (buffer[tcphdr_len+1] == 0x03) && (buffer[tcphdr_len+2] < 0x04) &&
     (buffer[tcphdr_len+9] == 0x03) && (buffer[tcphdr_len+10] < 0x04))    ||
-    (((buffer[tcphdr_len] < 0x14) ||
+    (((buffer[tcphdr_len] < 0x14) ||                            //SSLv2
     ((buffer[tcphdr_len] > 0x18) &&
     (buffer[tcphdr_len + 1] > 0x09))) &&
     (buffer[tcphdr_len+3] == 0x00) &&
-    (buffer[tcphdr_len+4] == 0x02)))
-    {
+    (buffer[tcphdr_len+4] == 0x02))) {
         ssl_connection(buffer, data_len, tcphdr_len, tcph->th_sport, tcph->th_dport, temp_src, temp_dest, time, microsec);
     }
 
     // FIN flag - TCP connection is closing
     // so SSL connection was closed as well
     // with the last SSL packet of that connection
-    if (tcph->th_flags & TH_FIN ) {
+    if (tcph->th_flags & TH_FIN) {
         print_data(tcph->th_sport, tcph->th_dport);
     }
     return 0;
@@ -400,34 +418,34 @@ void callback(u_char *args, const struct pcap_pkthdr* pkthdr,const u_char* buffe
 	signal(SIGINT, intHandler); 		//to properly catch CTRL+C
 	struct ether_header *p = (struct ether_header *) buffer;
 	bool ipv6 = false;
-	int tcp_switch = 0;
+	int tcp_switch;
 	if (ntohs(p->ether_type) == ETHERTYPE_IPV6) {     // if ETHERTYPE is IPV6, flag is set to true
         ipv6 = true;
     }
 	args = NULL; // for not having a warning of unused variable
     const unsigned int data_len = (pkthdr->len);
-    const u_char *data = (buffer);
-
 	//Get the IP Header part of this packet , excluding the ethernet header
-	if (ipv6 == true)
-	{
+	if (ipv6 == true) {
 		struct ip6_hdr *iph = (struct ip6_hdr *)(buffer + sizeof(struct ether_header));
 		tcp_switch = iph->ip6_ctlun.ip6_un1.ip6_un1_nxt;
 	}
-	else
-	{
+	else {
 		struct ip *iph = (struct ip*)(buffer + sizeof(struct ether_header));
 		tcp_switch = iph->ip_p;
 	}
-	switch (tcp_switch) //Check the Protocol and do accordingly... just to be sure, even when we have TCP filter
-	{
-		case 6:  //TCP Protocol
-			tcp_packet(pkthdr->ts.tv_sec, pkthdr->ts.tv_usec, buffer, ipv6, data_len);
-			break;
-	}
+
+	if (tcp_switch == 6) {  //TCP Protocol, just to be sure, even when we have TCP filter
+            tcp_packet(pkthdr->ts.tv_sec, pkthdr->ts.tv_usec, buffer, ipv6, data_len);
+    }
+
 }
 
-
+/*
+ * setup the connection either from the pcap/pcapng file
+ * or to the actual network device
+ * returns NULL on error
+ * returns pcap_t type on success
+ */
 pcap_t *set_up(int mode,char *iface,char *rfile)
 {
 	struct bpf_program fp;        // to hold compiled program
@@ -442,8 +460,7 @@ pcap_t *set_up(int mode,char *iface,char *rfile)
     if (iface) { iface_num = strtol(iface, &check, 10); }
     if (iface && check[0] == '\0') {
         pcap_if_t *iface_list;
-        if (pcap_findalldevs(&iface_list, errbuf) == -1)
-        {
+        if (pcap_findalldevs(&iface_list, errbuf) == -1) {
             fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
             return NULL;
         }
@@ -459,34 +476,37 @@ pcap_t *set_up(int mode,char *iface,char *rfile)
         }
     }
 
-	if (pcap_lookupnet(iface, &pNet, &pMask, errbuf) == -1)
-	{
+	if (pcap_lookupnet(iface, &pNet, &pMask, errbuf) == -1) {
 		fprintf(stderr, "%s\n", errbuf);
 		return NULL;
 	}
 
 	// Opens device for sniffing
-	if (mode == 0){     //help printed
-	    return NULL;
-	}
-	if (mode == 10) 
-		sniff = pcap_open_offline(rfile, errbuf); //https://www.tcpdump.org/manpages/pcap_loop.3pcap.html
-	else 
-		sniff = pcap_open_live(iface, BUFSIZ, 0, 1000, errbuf);
-
+	if (mode == 10) {   // -r -reading from pcapng file
+        char *file_check = realpath(rfile, NULL);
+        if (!file_check) {
+            fprintf(stderr, "File '%s' does not exist.\n", rfile);
+            return NULL;
+        }
+        sniff = pcap_open_offline(rfile, errbuf); //https://www.tcpdump.org/manpages/pcap_loop.3pcap.html
+    }
+	else {              // listen from the device
+        sniff = pcap_open_live(iface, BUFSIZ, 0, 1000, errbuf);
+    }
+	//
 	if(sniff == NULL)
 	{
 		fprintf(stderr, "pcap_open failed due to [%s]\n", errbuf);
 		return NULL;
 	}
 
+	//sets the filter only to TCP packets
 	//source: https://www.tcpdump.org/manpages/pcap_compile.3pcap.html
 	if(pcap_compile(sniff, &fp, "tcp ", 0, pNet) == -1)	
 	{
 		fprintf(stderr, "pcap_compile() failed\n");
 		return NULL;
 	}
-
 	if(pcap_setfilter(sniff, &fp) == -1)
 	{
 		fprintf(stderr, "pcap_setfilter() failed\n");
@@ -495,13 +515,19 @@ pcap_t *set_up(int mode,char *iface,char *rfile)
 	return sniff;
 }
 
+
+/*
+ * main body of the ssl monitor
+ * returns 0 on success
+ * returns 1 on malloc error
+ */
 int main(int argc, char *argv[])
 {
-    DLInitList(&connection_list);
+    DLInitList(&connection_list);                   //initialize the list for connections
 	char *iface = malloc(41 * sizeof(char));	//interface
-	char *rfile = malloc(201 * sizeof(char));		//pcapng file
-	if (iface == NULL) { return 1; }
-	if (rfile == NULL) { return 1; }
+	char *rfile = malloc(201 * sizeof(char));	//pcapng file
+	if (iface == NULL) { return 1; }                //malloc error
+	if (rfile == NULL) { return 1; }                //malloc error
     //TODO check file validity to be more robust
 	int args = args_parse(argc, argv, iface, rfile);
 	if (args == 1 || args == 0) {
@@ -521,20 +547,15 @@ int main(int argc, char *argv[])
 		iface = NULL;
 	}
 
-	// printf("interface=%s, pcapng file=%s\n", iface, rfile);
 	pcap_t *sniff = set_up(args, iface, rfile);
 	if (sniff) {
-        // Loop for catching packets, ends after pnum packets were catched
+        // Loop for catching packets, ends after EOF or keyboard interrupt
         pcap_loop(sniff, -1 , callback, NULL);
 	}
 
+	// deallocate the used memory
 	DLDisposeList(&connection_list);
-
-	if (iface) {
-		free(iface);
-	}
-	if (rfile) {
-		free(rfile);
-	}
+	if (iface) { free(iface); }
+	if (rfile) { free(rfile); }
 	return 0;
 }
